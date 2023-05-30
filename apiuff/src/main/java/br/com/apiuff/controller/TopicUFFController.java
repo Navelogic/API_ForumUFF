fddbba202e7a83088f85a65ceb1987900017592c
Arian Weslley <weslleysantoszbd@gmail.com>
2023-05-30 16:25:19 -0300

    RestController

--- apiuff/src/main/java/br/com/apiuff/controller/TopicUFFController.java
+++ apiuff/src/main/java/br/com/apiuff/controller/TopicUFFController.java
@@ -2,18 +2,16 @@ package br.com.apiuff.controller;
 
 import br.com.apiuff.entities.Course;
 import br.com.apiuff.entities.TopicUFF;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class TopicUFFController {
 
     @RequestMapping("/topics")
-    @ResponseBody
     public List<TopicUFF> list() {
         TopicUFF topic = new TopicUFF("Dúvida", "Dúvida com Spring", new Course("Spring", "Programação"));
         return Arrays.asList(topic, topic, topic);
