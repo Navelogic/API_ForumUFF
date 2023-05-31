fcb3a45b2d1137f95d5d223a46f1cf86911d4ab1
Arian Weslley <weslleysantoszbd@gmail.com>
2023-05-30 23:26:07 -0300

    MessageSource

--- apiuff/src/main/java/br/com/apiuff/config/ErrorHandlerValidation.java
+++ apiuff/src/main/java/br/com/apiuff/config/ErrorHandlerValidation.java
@@ -1,5 +1,9 @@
 package br.com.apiuff.config;
 
+import org.aspectj.bridge.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -9,9 +13,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestControllerAdvice
 public class ErrorHandlerValidation {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -19,7 +27,7 @@ public class ErrorHandlerValidation {
         List<ErrorFormDTO> dto = new ArrayList<>();
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
         fieldErrors.forEach(e -> {
-            String message = e.getDefaultMessage();
+            String message = messageSource.getMessage(e, LocaleContextHolder.getLocale());
             String field = e.getField();
             ErrorFormDTO error = new ErrorFormDTO(field, message);
             dto.add(error);
