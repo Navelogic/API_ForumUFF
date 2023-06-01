e36a3c53a18faa9b4490fdb731ba208f7a458a9a
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-01 11:04:51 -0300

    EnableCaching

--- apiuff/src/main/java/br/com/apiuff/ApiuffApplication.java
+++ apiuff/src/main/java/br/com/apiuff/ApiuffApplication.java
@@ -2,10 +2,12 @@ package br.com.apiuff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport // Pagination
+@EnableCaching // Cache
 public class ApiuffApplication {
 
     public static void main(String[] args) {
