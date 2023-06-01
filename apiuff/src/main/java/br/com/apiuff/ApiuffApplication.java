f712b63dc44bfb1c7881ee198665944cc67e3deb
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-01 10:22:25 -0300

    Update ApiuffApplication.java

--- apiuff/src/main/java/br/com/apiuff/ApiuffApplication.java
+++ apiuff/src/main/java/br/com/apiuff/ApiuffApplication.java
@@ -2,8 +2,10 @@ package br.com.apiuff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport // Pagination
 public class ApiuffApplication {
 
     public static void main(String[] args) {
