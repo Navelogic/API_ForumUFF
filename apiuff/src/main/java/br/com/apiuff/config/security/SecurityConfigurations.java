28a2cbdb038a1fd943b406a13f6779d44251117f
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-04 16:41:35 -0300

    authenticationManager

--- apiuff/src/main/java/br/com/apiuff/config/security/SecurityConfigurations.java
+++ apiuff/src/main/java/br/com/apiuff/config/security/SecurityConfigurations.java
@@ -2,6 +2,8 @@ package br.com.apiuff.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,5 +19,9 @@ public class SecurityConfigurations{
         return http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
--- apiuff/src/main/java/br/com/apiuff/controller/AuthController.java
+++ apiuff/src/main/java/br/com/apiuff/controller/AuthController.java
@@ -3,7 +3,10 @@ package br.com.apiuff.controller;
 
 import br.com.apiuff.controller.dto.DataAuth;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AuthController {
 
+    @Autowired
+    private AuthenticationManager manager;
+
     @PostMapping
     public ResponseEntity<?> login(@RequestBody @Valid DataAuth dataAuth){
+        var token = new UsernamePasswordAuthenticationToken(dataAuth.email(), dataAuth.password());
+        var auth = manager.authenticate(token);
+
         return ResponseEntity.ok().build();
     }
 
