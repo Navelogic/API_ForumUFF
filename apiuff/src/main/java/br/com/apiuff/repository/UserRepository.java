c15dd4b13c0df982fec36bc446d447d8b268c42a
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-04 16:07:40 -0300

    UserRepository

--- apiuff/src/main/java/br/com/apiuff/repository/UserRepository.java
+++ apiuff/src/main/java/br/com/apiuff/repository/UserRepository.java
@@ -2,7 +2,9 @@ package br.com.apiuff.repository;
 
 import br.com.apiuff.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    UserDetails findByEmail(String email);
 }
--- apiuff/src/main/java/br/com/apiuff/service/AuthService.java
+++ apiuff/src/main/java/br/com/apiuff/service/AuthService.java
@@ -1,5 +1,7 @@
 package br.com.apiuff.service;
 
+import br.com.apiuff.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -7,8 +9,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return userRepository.findByEmail(username);
     }
 }
