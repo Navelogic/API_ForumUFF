5536bb3c1946b39e50b835e50ff66469af837364
Arian Weslley <weslleysantoszbd@gmail.com>
2023-06-04 14:31:56 -0300

    Update SecurityConfig.java

--- apiuff/src/main/java/br/com/apiuff/config/security/SecurityConfig.java
+++ apiuff/src/main/java/br/com/apiuff/config/security/SecurityConfig.java
@@ -1,4 +1,12 @@
 package br.com.apiuff.config.security;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfiguration {
+    
+
 }
